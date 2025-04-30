55be74f1bb083d2e314345d6035a2d17cac15bc0
k-kiha <k-kiha@yonsei.ac.kr>
2025-04-30 16:33:03 +0900

    update

--- example_simple3d/main.f90
+++ example_simple3d/main.f90
@@ -9,7 +9,7 @@ program main
     integer :: n1sub,n2sub,n3sub
     real*8, allocatable, dimension(:,:,:) :: a,b,c,d
     integer :: mpiutil_para
-    integer :: timeA,timeB
+    real*8 :: timeA,timeB
 
     integer :: i, j, k
     integer :: indx_tmpa, indx_tmpb
