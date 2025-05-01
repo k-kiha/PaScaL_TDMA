dd984c5c75c2dcc7e710e5687b4d7fb3adf710aa
k-kiha <k-kiha@yonsei.ac.kr>
2025-05-01 23:27:31 +0900

    update

--- example_simple3d/build.sh
+++ example_simple3d/build.sh
@@ -1,2 +1,2 @@
-mpiifort -c main.f90 -I../include 
-mpiifort *.o -I../include -L../lib -lpascal_tdma
+mpiifort -O3 -fPIC -fp-model=precise -xMIC-AVX512 -c main.f90 -I../include 
+mpiifort -O3 -fPIC -fp-model=precise -xMIC-AVX512 *.o -I../include -L../lib -lpascal_tdma
